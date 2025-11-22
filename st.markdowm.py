c30ddcef66805246016b7930150a8773d503d480
Ramannagari Chakravardhan <chakravardhanramannagari06@gmail.com>
2025-11-22 12:01:45 +0530

    Add files via upload

---
+++ st.markdowm.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.markdown("# This is a Heading")     # Big text
+st.markdown("## This is Medium Heading")
+st.markdown("**Bold Text**")          # Bold
+st.markdown("*Italic Text*")          # Italic
+st.markdown("- Apple 🍎")              # Bullet list
+st.markdown("[Google](https://google.com)")   # Link
+
+### 🗯️ Styled Text → st.markdown("text")
